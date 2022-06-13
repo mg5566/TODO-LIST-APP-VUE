4ff1422724deca92239a05b703210f83818d1007
mg5566 <mg5566@hanmail.net>
2022-06-13 15:06:18 +0900

    1 todo form (#5)
    * fix: remove bad code
    
    * feat: add axios package
    
    * feat: add post feature
    
    * fix: remove day

--- todo-list-vue/src/components/TodoForm.vue
+++ todo-list-vue/src/components/TodoForm.vue
@@ -4,15 +4,18 @@
       <h3>New TODO</h3>
     </header>
     <label for="title">Title</label>
-    <input id="title" type="text" v-model="title" />
+    <input id="title" type="text" required v-model="title"/>
     <label for="description">Description</label>
-    <input id="description" type="text" v-model="description" />
+    <input id="description" type="text" required v-model="description"/>
     <button type="submit">ADD</button>
   </form>
 </template>
 
 <script>
 import calcDate from "@/utils/calcDate";
+import axios from "axios";
+
+const FIREBASE_DOMAIN = "https://todoappvue-d3b68-default-rtdb.firebaseio.com/";
 
 export default {
   data() {
@@ -24,9 +27,27 @@ export default {
   methods: {
     submitNewTodo() {
       console.log("Title", this.title, "Description", this.description);
+      // create todo item
+      const todoItem = {
+        id: this.title,
+        title: this.title,
+        description: this.description,
+        status: "TODO",
+        date: calcDate(),
+      }
+      // fetch new todo to Back-end DB
+      axios.post(`${FIREBASE_DOMAIN}/todos.json`, todoItem)
+          .then((response) =>
+              console.log("[SUCCESS]TODO POST", response)
+          )
+          .catch((error) =>
+              console.warn("[ERROR]TODO POST", error)
+          );
+
+      // set input to blank
       this.title = "";
-      this.description ="";
-      //  fetch new todo to Back-end DB
+      this.description = "";
+
       // todo spec
       /*
       todoItem = {
--- todo-list-vue/src/utils/calcDate.js
+++ todo-list-vue/src/utils/calcDate.js
@@ -3,15 +3,14 @@ const calcDate = () => {
   let today = new Date();
 
   let year = today.getFullYear();
-  let month = today.getMonth() + 0;
+  let month = today.getMonth() + 1;
   let date = today.getDate();
-  let day = today.getDay();
 
   let hours = today.getHours();
   let minutes = today.getMinutes();
   let seconds = today.getSeconds();
 
-  const result = year + "." + month + "." + date + "." + day + " " + hours + ":" + minutes +":" + seconds;
+  const result = year + "." + month + "." + date + ". " + hours + ":" + minutes +":" + seconds;
 
   return result;
 };
