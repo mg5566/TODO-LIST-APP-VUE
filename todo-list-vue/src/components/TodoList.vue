6af92850ae829e13c14e06453ed0d4730c8a5ad2
unknown <mg5566@hanmail.net>
2022-06-14 12:13:32 +0900

    feat: add card layout

--- todo-list-vue/src/components/TodoList.vue
+++ todo-list-vue/src/components/TodoList.vue
@@ -1,13 +1,15 @@
 <template>
   <ul>
     <li v-for="todo in todos" :key="todo.id">
-      <h2>TITLE - {{ todo.title }}</h2>
-      <div>
-        <p>DESCRIPTION</p>
-        <p>{{ todo.description }}</p>
-        <p>{{ todo.date }}</p>
-        <p>{{ todo.status }}</p>
-      </div>
+      <card-base>
+        <h2>TITLE - {{ todo.title }}</h2>
+        <div>
+          <p>DESCRIPTION</p>
+          <p>{{ todo.description }}</p>
+          <p>{{ todo.date }}</p>
+          <p>{{ todo.status }}</p>
+        </div>
+      </card-base>
     </li>
   </ul>
 </template>
@@ -28,17 +30,17 @@ export default {
           // transform
           const transformedData = [];
 
-          for(const key in data) {
+          for (const key in data) {
             const todoObj = {
               id: key,
               ...data[key],
-            }
+            };
 
             transformedData.push(todoObj);
           }
 
           this.todos = transformedData;
-        })
+        });
   },
   data() {
     return {
@@ -49,10 +51,9 @@ export default {
 </script>
 
 <style scoped>
-li {
-  list-style-type: none;
-  border: 1px solid #2c3e50;
-  border-radius: 16px;
-  margin: 1rem;
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
 }
 </style>
