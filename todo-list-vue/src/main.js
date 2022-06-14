d80a2bf46e9dd95646bf24323676741bb44b9328
unknown <mg5566@hanmail.net>
2022-06-14 12:19:02 +0900

    refactor: modify component name

--- todo-list-vue/src/main.js
+++ todo-list-vue/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import CardBase from "./components/UI/Card-Base"
+import CardBase from "./components/UI/CardBase"
 
 const app = createApp(App)
 
