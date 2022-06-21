d1745e9b94ebbbcac89debeeee418ec4f44c9df2
unknown <mg5566@hanmail.net>
2022-06-21 10:20:27 +0900

    feat: add button styling

--- todo-list-vue/src/components/HeaderTitle.vue
+++ todo-list-vue/src/components/HeaderTitle.vue
@@ -6,3 +6,30 @@
     <router-link to="/dones">Done List</router-link>
   </header>
 </template>
+
+<style scoped>
+header {
+  width: 100%;
+  height: 7rem;
+  background-color: gray;
+}
+
+a {
+  font: inherit;
+  text-decoration: none;
+  background: transparent;
+  border: 1px solid transparent;
+  cursor: pointer;
+  color: white;
+  padding: 0.5rem 1.5rem;
+  display: inline-block;
+}
+
+a:hover,
+a:active,
+a.router-link-active {
+  color: white;
+  border-color: #2c3e50;
+  background-color: #2c3e50;
+}
+</style>
